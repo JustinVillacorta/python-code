170ae88a3339a935cbe144e9457276062bc5dd9f
akane696969 <juvi.villacorta.up@phinmaed.com>
2023-09-16 21:25:29 +0800

    Enter

--- p1lab2.py
+++ p1lab2.py
@@ -1,7 +1,7 @@
-n1 = 4
-n2 = 3
-n3 = 4.5
-n4 = 5
+n1 = input(int("enter a number: "))
+n2 = input(int("enter a number: "))
+n3 = input(int("enter a number: "))
+n4 = input(int("enter a number: "))
 
 num = (n1*n2*n3*n4)**(0.25)
 print(round(num, 2))
